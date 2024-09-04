a7848cfdb87052bd5bf08fd8ac33e282ec2fd9ef
sohel <sohel@atilimited.net>
2024-09-04 18:05:39 +0600

    update status update notification

--- frontend/src/modals/StatusUpdate.vue
+++ frontend/src/modals/StatusUpdate.vue
@@ -9,35 +9,38 @@ import axios from 'axios'
 
 // for response notification
 const [api, contextHolder] = notification.useNotification()
-const openResponseNotification = (placement: NotificationPlacement, response: { responseStatus: boolean, responseUserName: string }) => {
-  const action = response.responseStatus === false ? 'deactivated' : 'activated';
+const openResponseNotification = (
+  placement: NotificationPlacement,
+  response: { responseStatus: boolean; responseUserName: string },
+) => {
+  const action = response.responseStatus === false ? 'deactivated' : 'activated'
   api.info({
     message: 'Response Notification',
     description: `${response.responseUserName} was ${action} successfully`,
     placement,
-  });
+  })
 }
 
 // Define emits
-const emit = defineEmits(['statusUpdate']);
+const emit = defineEmits(['statusUpdate'])
 const props = defineProps({
-  statusData: Object
+  statusData: Object,
 })
 
 const close = () => {
-  emit('statusUpdate', false);  // Emit event to parent with 'false' value
-};
+  emit('statusUpdate', false) // Emit event to parent with 'false' value
+}
 
 const openNotification = () => {
-  const key = '1';
+  const key = '1'
 
   const userData = {
     id: props.statusData?.id,
-    status: props.statusData?.status
-  };
+    status: props.statusData?.status,
+  }
 
-  const statusType = props.statusData?.status === 1 ? 'deactivate' : 'activate';
-  const userName = props.statusData?.name;
+  const statusType = props.statusData?.status === 1 ? 'deactivate' : 'activate'
+  const userName = props.statusData?.name
 
   notification.open({
     message: 'Confirmation',
@@ -55,22 +58,34 @@ const openNotification = () => {
             size: 'small',
             style: { marginRight: '8px' },
             onClick: async () => {
-              notification.close(key);
+              notification.close(key)
               try {
-                const response = await axios.patch('http://localhost:8000/api/user-status2', {
+                const response = await axios.patch('http://localhost:8000/api/user-status', {
                   id: userData.id,
                   status: userData.status,
-                });
+                })
 
                 if (response.data.status === 200 || response.data.status === 300) {
                   const responseData = {
                     responseStatus: response.data.success.status,
-                    responseUserName: response.data.success.name
-                  };
-                  openResponseNotification('topRight', responseData);
+                    responseUserName: response.data.success.name,
+                  }
+                  openResponseNotification('topRight', responseData)
                   setTimeout(() => {
-                    emit('statusUpdate', false);
-                  }, 5000);
+                    emit('statusUpdate', false)
+                  }, 5000)
+                } else {
+                  if (response.data.status === 500) {
+                    console.log(response.data)
+                    const responseData = {
+                      responseStatus: response.data.message,
+                      responseUserName: response.data.error,
+                    }
+                    openResponseNotification('topRight', responseData)
+                    setTimeout(() => {
+                      emit('statusUpdate', false)
+                    }, 5000)
+                  }
                 }
               } catch (error) {
                 console.error(error)
@@ -89,8 +104,8 @@ const openNotification = () => {
             type: 'default',
             size: 'small',
             onClick: () => {
-              notification.close(key);
-              emit('statusUpdate', false);  // Emit event to parent with 'false' value
+              notification.close(key)
+              emit('statusUpdate', false) // Emit event to parent with 'false' value
             },
           },
           { default: () => 'No' },
@@ -98,10 +113,10 @@ const openNotification = () => {
       ]),
     key,
     onClose: close,
-  });
-};
+  })
+}
 
 onMounted(() => {
-  openNotification();
-});
+  openNotification()
+})
 </script>
